a3482614c0378d52b3cb1d59c18825cd2e25dd25
MELISSA MAZIR <melissamazir07@gmail.com>
2023-05-30 16:16:26 +0100

    adding styling and the remobe button

--- script.js
+++ script.js
@@ -11,37 +11,37 @@ class Book {
 
 class MyBookList {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem('books')) || [];
-    this.MyBookList = document.getElementById('my-book-list');
-    this.addForm = document.getElementById('my-book-form');
+    this.books = JSON.parse(localStorage.getItem("books")) || [];
+    this.MyBookList = document.getElementById("my-book-list");
+    this.addForm = document.getElementById("my-book-form");
 
-    this.addForm.addEventListener('submit', this.handleAddMyBook.bind(this));
-    this.MyBookList.addEventListener('click', this.handleRemoveBook.bind(this));
+    this.addForm.addEventListener("submit", this.handleAddMyBook.bind(this));
+    this.MyBookList.addEventListener("click", this.handleRemoveBook.bind(this));
     this.displayBooks();
   }
 
   handleAddMyBook(event) {
     event.preventDefault();
 
-    const titleInput = document.getElementById('title');
-    const authorInput = document.getElementById('author');
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
 
     const title = titleInput.value.trim();
 
     const author = authorInput.value.trim();
 
-    if (title === '' || author === '') {
-      const alertDiv = document.createElement('p');
-      alertDiv.innerHTML = 'Enter your title and book.';
-      const addForm = document.getElementById('my-book-form');
+    if (title === "" || author === "") {
+      const alertDiv = document.createElement("p");
+      alertDiv.innerHTML = "Enter your title and book.";
+      const addForm = document.getElementById("my-book-form");
       addForm.appendChild(alertDiv);
       return;
     }
 
     this.addBook(title, author);
 
-    titleInput.value = '';
-    authorInput.value = '';
+    titleInput.value = "";
+    authorInput.value = "";
   }
 
   // adding book
@@ -50,12 +50,12 @@ class MyBookList {
     const book = new Book(title, author);
     this.books.push(book);
 
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
     this.displayBooks();
   }
 
   handleRemoveBook(event) {
-    if (event.target.classList.contains('remove-btn')) {
+    if (event.target.classList.contains("remove-btn")) {
       const index = parseInt(event.target.dataset.index, 10);
       this.removeBook(index);
     }
@@ -65,7 +65,7 @@ class MyBookList {
 
   removeBook(index) {
     this.books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
 
     this.displayBooks();
   }
@@ -73,9 +73,9 @@ class MyBookList {
   // display
 
   displayBooks() {
-    this.MyBookList.innerHTML = '';
+    this.MyBookList.innerHTML = "";
     this.books.forEach((book, index) => {
-      const newDiv = document.createElement('div');
+      const newDiv = document.createElement("div");
 
       newDiv.innerHTML = `<div>${book.title}</div><div class="by">by</div> <div class="authors">${book.author}</div><div><button data-index="${index}" class="remove-btn">Remove</button></div><div><hr></div>`;
 
